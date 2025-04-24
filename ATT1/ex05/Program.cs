fb7b3b37a0931069b60aa0872dce588ac67d5261
Thiago Amaral Costa <thgamrlcsta@gmail.com>
2025-04-24 20:15:15 -0300

    Resolvido exercícios 4, 5 e 6

--- ATT1/ex05/Program.cs
+++ ATT1/ex05/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Verificador de palíndromos");
+        Console.WriteLine("Digite uma palavra ou frase:");
+        string input = Console.ReadLine();
+        string cleanedInput = RemoveNonAlphanumeric(input).ToLower();
+        bool isPalindrome = IsPalindrome(cleanedInput);
+
+        if (isPalindrome)
+        {
+            Console.WriteLine("A frase ou palavra é um palíndromo.");
+        }
+        else
+        {
+            Console.WriteLine("A frase ou palavra não é um palíndromo.");
+        }
+    }
+    static string RemoveNonAlphanumeric(string str)
+    {
+        char[] arr = str.ToCharArray();
+        string result = "";
+        foreach (char c in arr)
+        {
+            if (Char.IsLetterOrDigit(c))
+            {
+                result += c;
+            }
+        }
+        return result;
+    }
+    static bool IsPalindrome(string str)
+    {
+        int len = str.Length;
+        for (int i = 0; i < len / 2; i++)
+        {
+            if (str[i] != str[len - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- ATT1/ex06/Program.cs
+++ ATT1/ex06/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Produto
+{
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int Quantidade { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Produto> produtos = new List<Produto>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"Cadastro do produto {i + 1}:");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            decimal preco;
+            while (!decimal.TryParse(Console.ReadLine(), out preco))
+            {
+                Console.Write("Por favor, insira um preço válido: ");
+            }
+
+            Console.Write("Quantidade: ");
+            int quantidade;
+            while (!int.TryParse(Console.ReadLine(), out quantidade))
+            {
+                Console.Write("Por favor, insira uma quantidade válida: ");
+            }
+
+            produtos.Add(new Produto { Nome = nome, Preco = preco, Quantidade = quantidade });
+            Console.WriteLine();
+        }
+
+        decimal totalEstoque = 0;
+        Console.WriteLine("Produtos cadastrados:");
+        foreach (var produto in produtos)
+        {
+            decimal valorTotal = produto.Preco * produto.Quantidade;
+            totalEstoque += valorTotal;
+            Console.WriteLine($"Nome: {produto.Nome}, Preço: {produto.Preco:C}, Quantidade: {produto.Quantidade}, Valor Total: {valorTotal:C}");
+        }
+
+        Console.WriteLine($"\nValor total em estoque: {totalEstoque:C}");
+    }
+}
